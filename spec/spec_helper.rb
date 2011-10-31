aea52fba2744c3e7eb95dcd9fea08e68938a57c3
Hendrik Louw <hflouw@gmail.com>
2011-10-31 20:46:01 +0200

    setup rspec with a pending spec for example purpose

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+require 'SUPERvideo'
+
+Rspec.configure do |config|
+  config.color_enabled = true
+  config.formatter     = 'documentation'
+end
---
+++ spec/video_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe SuperVideo::Video do
+  it 'should create a new video object from a filename' do
+    pending 'implement Video.new filename: filename.videoformat spec'
+  end
+end
