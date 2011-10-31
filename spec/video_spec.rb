7f9bab4f7a7e4e01d335bcc9a1d4361923cb270f
Hendrik Louw <hflouw@gmail.com>
2011-10-31 20:55:10 +0200

    can create a new video

--- spec/video_spec.rb
+++ spec/video_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe SuperVideo::Video do
   it 'should create a new video object from a filename' do
-    pending 'implement Video.new filename: filename.videoformat spec'
+    video_input_name = 'test_file.mpeg'
+    video = SuperVideo::Video.new filename: video_input_name
+    video.filename.should == video_input_name
   end
 end
