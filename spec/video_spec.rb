181e0b13c85b34517bfb791e69ab5c54f660ff6a
Peter Garbers <peter.garbers@gmail.com>
2011-10-31 20:56:29 +0200

    made specs

--- spec/video_spec.rb
+++ spec/video_spec.rb
@@ -4,4 +4,24 @@ describe SuperVideo::Video do
   it 'should create a new video object from a filename' do
     pending 'implement Video.new filename: filename.videoformat spec'
   end
+
+  it "should convert a video to ogv" do
+    pending 'Video.new filename: filename.ogv'
+  end
+
+  it "should convert a video to mp4" do
+    pending 'Video.new filename: filename.mp4'
+  end
+
+  it "should convert a video to vp8" do
+    pending 'Video.new filename: filename.vp8'
+  end
+
+  it "should convert a video all the video formats" do
+    pending
+  end
+
+  it "should allow the user to specify a path" do
+    pending 'convert to: gv, output_base_filename: icecream.ogv'
+  end
 end
