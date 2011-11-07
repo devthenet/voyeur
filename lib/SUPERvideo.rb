1dc7919033e62edcdda8263d321567866d143890
Hendrik Louw <hflouw@gmail.com>
2011-11-07 22:10:17 +0200

    fix peter's gay mistake

--- lib/SUPERvideo.rb
+++ lib/SUPERvideo.rb
@@ -3,6 +3,8 @@ require "SUPERvideo/video"
 require "SUPERvideo/video_converter"
 require "SUPERvideo/video_converters/ogv_converter"
 require "SUPERvideo/video_converters/mp4_converter"
+require "SUPERvideo/video_converters/webm_converter"
+
 module SuperVideo
   attr_accessor :bitrate
 end
