c905c34dc28037ffa6d5ea1ce1ed2b565e0151a6
Peter Garbers <peter.garbers@gmail.com>
2011-11-09 21:20:33 +0200

    raise proper exception if there is no video

--- lib/Voyeur/video_converter.rb
+++ lib/Voyeur/video_converter.rb
@@ -7,35 +7,36 @@ module Voyeur
     def self.create(options)
       constant = Voyeur
       klass = "#{options[:format].capitalize}Converter"
+
       klass = constant.const_get(klass)
       klass.new
     end
 
     def convert(options)
       @input_video = options[:video]
-      raise "Please specify a video to convert!" if @input_video.nil?
-      
+      raise Voyeur::Exceptions::NoVideoPresent unless @input_video
+
       @output_video = Video.new(filename: self.output_file_name(@input_video.filename))
       self.call_external_converter
     end
-    
+
     def output_file_name(input_file_name)
       input_file_name.split('.').first + "." + self.file_extension
     end
-    
+
     protected
     def call_external_converter
       command = "ffmpeg -i #{@input_video.filename} #{self.convert_options} #{@output_video.filename}"
-      
+
       out, err = ""
-      
+
       status = Open4::popen4(command) do |pid, stdin, stdout, stderr|
         out = stdout.read.strip
         err = stderr.read.strip
       end
-      
+
       error_message = err.split('\n').last
-      
+
       @status = { status: status.exitstatus, stdout: out, stderr: err,
                   error_message: error_message, video: @output_video }
       return @status
