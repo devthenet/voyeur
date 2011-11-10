641c4c6ef9e22e1171a86108557b7800d42fc622
Hendrik Louw <hflouw@gmail.com>
2011-11-10 22:04:45 +0200

    real video testing

--- spec/spec_helpers/video_file_spec_helper.rb
+++ spec/spec_helpers/video_file_spec_helper.rb
@@ -1,15 +1,19 @@
 module SpecHelper
   module VideoFileSpecHelper
     def current_path
-      File.dirname(__FILE__)
+      File.join( File.dirname(__FILE__), "../" )
     end
     
     def fixture_file_path
-      File.join(current_path, "spec", "fixtures")
+      File.join(current_path, "fixtures")
     end
     
     def valid_3g_file_path
       File.join(fixture_file_path, "test.3gp")
+    end
+    
+    def valid_mpeg_file_path
+      File.join(fixture_file_path, "test.mpeg")
     end
   end
 end
