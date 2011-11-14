7b21529d404f3e8e1963ed482e6853c22b67993c
Peter Garbers <peter.garbers@gmail.com>
2011-11-14 18:40:44 +0200

    forking working with open4

--- spec/converters/mp4_converter_spec.rb
+++ spec/converters/mp4_converter_spec.rb
@@ -1,24 +1,29 @@
 require 'spec_helper'
 
 describe Voyeur::Mp4Converter do
-  context "A valid Video" do
-    before :each do
-      @converter = Voyeur::VideoConverter.create(format: :mp4)
-      @video = Voyeur::Video.new(filename: valid_mpeg_file_path)
-    end
+  before :each do
+    @converter = Voyeur::VideoConverter.create(format: :mp4)
+    @video = Voyeur::Video.new(filename: valid_mpeg_file_path)
+  end
 
-    it "should use the correct factory" do
-      @converter.class.to_s.should == "Voyeur::Mp4Converter"
+  it "should use the correct factory" do
+    @converter.class.to_s.should == "Voyeur::Mp4Converter"
+  end
+
+  it "default convert string" do
+    @converter.convert_options.should == "-b 1500k -vcodec libx264 -g 30"
+  end
+
+  context "A valid Video" do
+
+    after :each do
+      File.delete(valid_mpeg_file_path.gsub(/mpeg/, "mp4")) if File.exists?(valid_mpeg_file_path.gsub(/mpeg/, "mp4"))
     end
 
     context "#convert_options" do
-      it "default convert string" do
-        @converter.convert_options.should == "-b 1500k -vcodec libx264 -g 30"
-      end
-
       it "should name the video correctly" do
         @converter.convert(video: @video)
-        output_file = valid_mpeg_file_path.gsub(/.mpeg/, ".mp4")
+        output_file = valid_mpeg_file_path.gsub(/mpeg/, "mp4")
         @converter.output_video.filename.should == output_file
       end
 
@@ -29,14 +34,13 @@ describe Voyeur::Mp4Converter do
 
       context "real video" do
         it "should return conversion status indicating success" do
-          pending "should return the status"
           result = @converter.convert(video: @video)
           result[:status].should == 0
           result[:video].should == @converter.output_video
         end
 
         it "should return stdout" do
-          pending "should return the result"
+          pending "why do we want this? this is returning ffmpeg version stuff"
           result = @converter.convert(video: @video)
           result[:stderr].should == ""
           result[:stdout].should == ""
@@ -53,7 +57,6 @@ describe Voyeur::Mp4Converter do
 
     context "File does not exist" do
       it "should return conversion status indicating failure" do
-        pending "should return the status"
         result = @converter.convert(video: @video)
         result[:status].should == 1
         result[:video].should == @converter.output_video
