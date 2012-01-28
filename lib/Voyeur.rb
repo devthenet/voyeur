f4c655f4707f36ed030e2ef47f5144f1bfd64476
Marcel Fahle <m.fahle@gmail.com>
2012-01-27 19:10:53 -0500

    lib/Voyeur.rb and lib/voyeur.rb conflicted and resulted id a uninitialized constand nameerror

--- lib/Voyeur.rb
+++
@@ -1,8 +0,0 @@
-require "Voyeur/version"
-require "Voyeur/video"
-require "Voyeur/exceptions"
-require "Voyeur/converter"
-require "Voyeur/video_converters/ogv"
-require "Voyeur/video_converters/mp4"
-require "Voyeur/video_converters/webm"
-require 'open4'
