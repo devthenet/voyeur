a330a12de8af0f833c8bf3e6c50af741f5467418
Peter Garbers <peter.garbers@gmail.com>
2012-02-10 00:05:55 +0200

    Updated readme

---
+++ lib/Voyeur/media.rb
@@ -0,0 +1,23 @@
+module Voyeur
+  class Media
+    attr_reader :filename
+    def initialize(options)
+      @filename = options[:filename]
+    end
+
+    def convert(options)
+      converter = Voyeur::Converter.create(format: options[:to])
+      converter.convert(media: self,
+                        output_filename: options[:output_filename],
+                        output_path: options[:output_path])
+    end
+
+    def convert_to_html5(options = nil)
+      [:mp4, :ogv, :webm].each do |f|
+        options_hash = {to: f}
+        options_hash.merge!(options) if options
+        convert(options_hash)
+      end
+    end
+  end
+end
